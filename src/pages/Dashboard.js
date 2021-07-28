9cb71f9f1dd58e93f647738c014b2dbefabadaec
Vilay Bende <71513950+vilay1702@users.noreply.github.com>
2021-07-29 02:15:03 +0530

    added dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,16 @@
-// Continue from here 
-// video 248: Login Page 
-
 import React from 'react'
+import { Info, Repos, User, Search, Navbar } from '../components';
+import {loadingImage} from '../images/githubLogin.png';
 
 const Dashboard = () => {
     return (
-        <div>
-            
-        </div>
+        <main>
+            <Navbar></Navbar>
+            <Search />
+            <Info />
+            <User />
+            <Repos />
+        </main>
     )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import styled from 'styled-components';
-import loginImg from '../images/login-img.svg';
+import loginImg from '../images/githubLogin.png';
 const Login = () => {
   return (
   <Wrapper>
@@ -14,6 +14,7 @@ const Login = () => {
   );
 };
 const Wrapper = styled.section`
+  background: linear-gradient(to top ,#B5FCFE, #FFFFFF);
   min-height: 100vh;
   display: grid;
   place-items: center;
@@ -23,7 +24,10 @@ const Wrapper = styled.section`
     text-align: center;
   }
   img {
-    margin-bottom: 2rem;
+    margin: auto;
+    width: auto;
+    height: 50vh;
+    // margin-bottom: 2rem;
   }
   h1 {
     margin-bottom: 1.5rem;
